68b78f44ad1ed69ad1afed130c12ef7a123e4cc4
AleksandrKayuda <akayuda999@gmail.com>
2022-10-02 13:09:36 +0400

    Create taskApi.ts

---
+++ src/API/taskApi.ts
@@ -0,0 +1,18 @@
+import {instance} from "./axios/axiosInstance";
+
+export const taskApi = {
+    getTasks: (todolistId: string) => {
+        return instance.get(`/todo-lists/${todolistId}/tasks`).then(res => res.data)
+
+    },
+    createTask: (todolistId: string, title: string) => {
+        return instance.post(`/todo-lists/${todolistId}/tasks`, {title}).then(res => res.data)
+    },
+    deleteTask: (todolistId: string, taskId: string) => {
+        return instance.delete(`/todo-lists/${todolistId}/tasks/${taskId}`)
+
+    },
+    updateTask: (todolistId: string, taskId: string, title: string) => {
+        return instance.put(`/todo-lists/${todolistId}/tasks/${taskId}`, {title})
+    }
+}
