d4a88ff095295c0eec3d4398955d166d06bacf29
Aleksandr Kaiuda <akayuda93@gmail.com>
2022-10-28 12:23:08 +0400

    delete React

--- src/Components/Todolist/index.tsx
+++ src/Components/Todolist/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect } from 'react';
+import { ReactElement, useEffect } from 'react';
 
 import { Delete } from '@mui/icons-material';
 import { Button, IconButton } from '@mui/material';
